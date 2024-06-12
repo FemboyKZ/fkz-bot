af9713658504754b06b4c07f1d58e019c6063820
Dots <dotvnl@gmail.com>
2024-06-12 22:21:45 +0300

    remove extras

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,25 +1,7 @@
-const mongoose = require("mongoose");
-const mongodbURL = process.env.MONGODBURL;
-
-mongoose.set("strictQuery", false);
-
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
     console.log("Ready!");
-
-    if (!mongodbURL) return console.log("Setup mongodbURL in .env");
-
-    try {
-      await mongoose.connect(mongodbURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-
-      console.log("The MongoDB Database is running.");
-    } catch (error) {
-      console.error("Error connecting to MongoDB:", error);
-    }
   },
 };
